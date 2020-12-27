16ef1971cb3c61a8a8403009be2a98c2a0ef4523
Kunwoo Yoon <ykw0628@gmail.com>
2020-12-27 12:51:33 -0500

    habit-tracker

---
+++ src/components/habits.jsx
@@ -0,0 +1,48 @@
+import React, { PureComponent } from 'react';
+import Habit from './habit';
+import HabitAddForm from './habitAddForm';
+
+class Habits extends PureComponent {
+
+
+    handleIncrement =(habit)=>{
+        this.props.onIncrement(habit);
+      };
+      handleDecrement =(habit)=>{
+        this.props.onDecrement(habit);
+      };
+      handleDelete = (habit) =>{
+        this.props.onDelete(habit);
+      };
+      handleAdd = name =>{
+        this.props.onAdd(name);
+      };
+    render() {
+        return (
+          <div className = "habits">
+          <HabitAddForm onAdd = {this.handleAdd} />
+          <ul>
+               {
+                   this.props.habits.map(habit => (
+                       <Habit
+                        key = {habit.id} 
+                        habit = {habit} 
+                        count = {habit.count}
+                        onIncrement = {this.handleIncrement}
+                        onDecrement = {this.handleDecrement}
+                        onDelete = {this.handleDelete}
+                        onAdd = {this.handleAdd}
+                     />
+                   ))
+               }
+           </ul>
+           <button className="habits-reset" onClick = {this.props.onReset}>
+             Reset All
+           </button>
+          </div>
+           
+        );
+    }
+}
+
+export default Habits;
